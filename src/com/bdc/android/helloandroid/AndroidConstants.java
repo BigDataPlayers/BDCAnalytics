51f753fd0bd99aa23c78137d2ed2ee2cfeb263f7
BigDataPlayers <bigdataplayers@gmail.com>
2013-02-01 09:19:03 -0500

    Constants updated
    Signed-off-by: BigDataPlayers <bigdataplayers@gmail.com>

--- src/com/bdc/android/helloandroid/AndroidConstants.java
+++ src/com/bdc/android/helloandroid/AndroidConstants.java
@@ -4,9 +4,13 @@ package com.bdc.android.helloandroid;
  * Created with IntelliJ IDEA.
  * User: a09726a
  * Date: 1/30/13
- * Time: 5:38 PM
+ * Time: 1:12 PM
  * To change this template use File | Settings | File Templates.
  */
 public class AndroidConstants {
-    public static final String ANALYTICS_SERVER = "";
+
+    //Govind's box - http://192.168.6.23:8080
+    //AWS - http://ec2-23-22-121-65.compute-1.amazonaws.com
+    //localhost - http://192.168.6.65:8080
+    public static final String ANALYTICS_SERVER = "http://ec2-23-22-121-65.compute-1.amazonaws.com";
 }
